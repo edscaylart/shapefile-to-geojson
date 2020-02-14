01d5e0309fb7c418ad40b6f0bbbcd81f3967f61c
Edson Souza <ed_f_souza@hotmail.com>
2020-02-14 15:24:17 -0400

    add readme..md

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/file-upload', methods=['POST'])
+@app.route('/convert', methods=['POST'])
 def upload_file():
   file = request.files['file']
   uploadfilename = secure_filename(file.filename)
